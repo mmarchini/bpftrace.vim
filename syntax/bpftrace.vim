mmarchini/bpftrace.vim

commit b7e9c710488622fc00cf1417e05298ebd2b65934

Improve number matching
There are no actual floats in bpftrace, but integers can be specified in
scientific notation.  Numbers are allowed to contain underscores.

syntax/bpftrace.vim
@@ -104,10 +104,14 @@ syntax match  bpftraceInclude   display "^\s*\zs\(%:\|#\)\s*include\>\s*["<]" co
 " From c.vim
 syntax region bpftraceDefine start="^\s*\zs\(%:\|#\)\s*\(define\|undef\)\>" skip="\\$" end="$" keepend contains=ALLBUT,@cPreProcGroup,@Spell
 
-syntax match bpftraceNumber "\v<\d+>"
-syntax match bpftraceNumber "\v<\d+\.\d+>"
-syntax match bpftraceNumber "\v<\d*\.?\d+([Ee]-?)?\d+>"
-syntax match bpftraceNumber "\v<0x\x+([Pp]-?)?\x+>"
+" Decimal number possibly containing underscores
+syntax match bpftraceNumber "\v<[1-9]((\d|_)*\d)?>"
+" Scientifc integer
+syntax match bpftraceNumber "\v<\d[eE]\d((\d|_)*\d)?>"
+" Octal number possibly containg underscores
+syntax match bpftraceNumber "\v<0(\o|_)*\o>"
+" Hex number
+syntax match bpftraceNumber "\v<0[xX]\x+>"
 
 " From c.vim
 syntax  match bpftraceFormat display "%\(\d\+\$\)\=[-+' #0*]*\(\d*\|\*\|\*\d\+\$\)\(\.\(\d*\|\*\|\*\d\+\$\)\)\=\([hlLjzt]\|ll\|hh\)\=\([aAbdiuoxXDOUfFeEgGcCsSpn]\|\[\^\=.[^]]*\]\)" contained

commit d7f037a2dae44727a623d4bbe99808ecf12b693e

Un-break hilighting of digit 0
Instead of matching decimal and octal separately, don't try to be
clever.

Fixes: b7e9c7104886

syntax/bpftrace.vim
@@ -142,12 +142,10 @@ syntax match  bpftraceInclude   display "^\s*\zs\(%:\|#\)\s*include\>\s*["<]" co
 " From c.vim
 syntax region bpftraceDefine start="^\s*\zs\(%:\|#\)\s*\(define\|undef\)\>" skip="\\$" end="$" keepend contains=ALLBUT,@cPreProcGroup,@Spell
 
-" Decimal number possibly containing underscores
-syntax match bpftraceNumber "\v<[1-9]((\d|_)*\d)?>"
+" Decimal or octal number possibly containing underscores
+syntax match bpftraceNumber "\v<\d((\d|_)*\d)?>"
 " Scientifc integer
 syntax match bpftraceNumber "\v<\d[eE]\d((\d|_)*\d)?>"
-" Octal number possibly containg underscores
-syntax match bpftraceNumber "\v<0(\o|_)*\o>"
 " Hex number
 syntax match bpftraceNumber "\v<0[xX]\x+>"
 